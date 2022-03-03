4f8cd40ee8b78263c3a787ffb4a871f49c4d96d3
kochtaik <kochtaik@gmail.com>
2022-03-03 14:54:18 +0300

    fix tests

--- tests/unit/Layouts.spec.ts
+++ tests/unit/Layouts.spec.ts
@@ -3,13 +3,18 @@ import AppLayout from "@/layouts/AppLayout.vue";
 import BaseHeader from "@/components/UI/BaseHeader.vue";
 import BaseFooter from "@/components/UI/BaseFooter.vue";
 import { mount } from "@vue/test-utils";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { createStore } from "vuex";
+import { key } from "@/store";
 
 jest.mock('vue-router', () => ({
   useRoute: jest.fn(),
+  useRouter: jest.fn(() => ({
+    push: () => { }
+  }))
 }));
-  
-test('has components inside', ()  => {
+
+test('has components inside', () => {
 
   // @ts-ignore
   useRoute.mockImplementationOnce(() => ({
@@ -18,9 +23,24 @@ test('has components inside', ()  => {
     },
   }));
 
+  const push = jest.fn();
+  // @ts-ignore
+  useRouter.mockImplementationOnce(() => ({ push }));
+
+  const store = createStore({
+    getters: {
+      isLoggedIn() {
+        return true;
+      }
+    }
+  });
+
   const wrapper = mount(App, {
     global: {
       stubs: ['router-link', 'router-view'],
+      provide: {
+        [key as symbol]: store,
+      }
     },
   });
 
