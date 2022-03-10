c0abe8ee1163350fa54a87690db82bda19813d91
kochtaik <kochtaik@gmail.com>
2022-03-10 18:13:23 +0300

    add test to check redirection to homepage

--- tests/unit/auth/SignIn.spec.ts
+++ tests/unit/auth/SignIn.spec.ts
@@ -2,11 +2,13 @@ import SignIn from "@/components/auth/SignIn.vue";
 import { mount } from "@vue/test-utils";
 import { key } from "@/store";
 import * as toast from "@/plugins/customToasts";
+import { useRouter } from "vue-router";
 
 const authParams = {
   email: "admin123@test.com",
   password: 'admin123',
 };
+const push = jest.fn();
 
 jest.mock('vue-router', () => ({
   useRouter: jest.fn(() => ({
@@ -19,8 +21,15 @@ let store: {
   commit: jest.Mock<any, any>;
   dispatch: jest.Mock<any, any>;
 };
+let form: ReturnType<typeof wrapper.get>;
 
-beforeEach(() => {
+beforeEach(async () => {
+  jest.clearAllMocks();
+  //@ts-ignore
+  useRouter.mockImplementationOnce(() => ({
+    push
+  }));
+
   store = {
     commit: jest.fn(),
     dispatch: jest.fn(),
@@ -31,19 +40,19 @@ beforeEach(() => {
       provide: {
         [key as symbol]: store,
       },
-      stubs: ['router-link', 'BaseInput'],
+      stubs: ['router-link', 'router-view', 'BaseInput', 'BaseButton'],
     },
   });
-});
 
-
-it('sets a user to Vuex after a successful authentication', async () => {
   const emailField = wrapper.findComponent('#email');
   const passwordField = wrapper.findComponent('#password');
-  const form = wrapper.get('[data-test="signInForm"]');
+  form = wrapper.get('[data-test="signInForm"]');
 
   await emailField.setValue(authParams.email);
   await passwordField.setValue(authParams.password);
+});
+
+it('sets a user to Vuex after a successful authentication', async () => {
   await form.trigger('submit');
 
   expect(store.dispatch).toHaveBeenCalled();
@@ -53,17 +62,16 @@ it('shows a toast when error occurs', async () => {
   store.dispatch = jest.fn(() => {
     throw new Error;
   });
-
   //@ts-ignore
   toast.error = jest.fn();
 
-  const emailField = wrapper.findComponent('#email');
-  const passwordField = wrapper.findComponent('#password');
-  const form = wrapper.get('[data-test="signInForm"]');
+  await form.trigger('submit');
+  expect(toast.error).toBeCalled();
+});
 
-  await emailField.setValue(authParams.email);
-  await passwordField.setValue(authParams.password);
+it('redirects user to the home page after successful authentication', async () => {
   await form.trigger('submit');
 
-  expect(toast.error).toBeCalled();
+  expect(push).toHaveBeenCalledTimes(1);
+  expect(push).toHaveBeenCalledWith('/');
 });
